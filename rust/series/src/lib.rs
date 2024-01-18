a3f8ad982fc31c80368ad6bce6e1f79a6d0ec325
Felipe Marques de Souza <felipehost@hotmail.com>
2024-01-18 20:39:00 -0300

    Lang: Rust - Exercise: Series - Easy - Alt

--- rust/series/src/lib.rs
+++ rust/series/src/lib.rs
@@ -1,17 +1,12 @@
 pub fn series(digits: &str, len: usize) -> Vec<String> {
-    let mut vec_result: Vec<String> = Vec::new();
-
     if len > digits.len() || digits.len() == 0 {
-        return vec_result
+        return Vec::new()
     }
 
-    for (index, _digit) in digits.char_indices() {
-        vec_result.push(digits[index..len+index].to_string());
-
-        if len + index == digits.len() {
-            break;
-        }
-    }
-
-    vec_result
+    digits
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(len)
+        .map(|s| s.iter().collect::<String>())
+        .collect()
 }
